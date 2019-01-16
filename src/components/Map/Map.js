c9d3df52019b78b9ca9d958646e5c86edc3eb774
David Biles <dlbiles1994@gmail.com>
2019-01-16 11:46:53 -0600

    routing through route redux

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { compose, withProps, lifecycle } from "recompose";
 import { withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer } from 'react-google-maps';
+import { connect } from 'react-redux';
 import './Map.css'
 const google = window.google;
 
@@ -16,18 +17,32 @@ const DirectionsMap = compose(
   lifecycle({
     componentDidMount() {
       const DirectionsService = new google.maps.DirectionsService();
+      
+      // creating Waypoints through Route redux
+      const { route } = this.props.route
+      const origin = route[0]
+      const destination = route[route.length-1]
+      const filterRoute = route.filter((e,i) => {
+        if( i !== 0 && i !== route.length-1 ){
+          return e
+        }
+      })
+      const waypoints = filterRoute.map((e,i) => {
+        return {location: new google.maps.LatLng({lat: e.lat, lng: e.lng})}
+      })
 
+      // This displays on the map the Routing
       DirectionsService.route({
-        origin: new google.maps.LatLng({ lat: 33.1345146, lng: -94.96463709999999 }),
-        waypoints: [{location: new google.maps.LatLng({lat: 30.5986668, lng: -97.41751909999999})}],
-        destination: new google.maps.LatLng({ lat: 31.2997975, lng: -94.7313099 }),
+        origin: new google.maps.LatLng({ lat: origin.lat, lng: origin.lng }),
+        optimizeWaypoints: false,
+        waypoints: waypoints,
+        destination: new google.maps.LatLng({ lat: destination.lat, lng: destination.lng }),
         travelMode: google.maps.TravelMode.DRIVING,
       }, (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
           this.setState({
             directions: result,
           });
-          console.log(result)
         } else {
           console.error(`error fetching directions ${result}`);
         }
@@ -43,13 +58,16 @@ const DirectionsMap = compose(
   </GoogleMap>
 );
 
-class Map extends Component {
 
+class Map extends Component {
+  
   render() { 
     return ( 
-      <DirectionsMap />
-    );
+      <DirectionsMap route={this.props.route} />
+      );
+    }
   }
-}
-
-export default Map;
+  
+  const mapStateToProps = state => state
+  
+export default connect(mapStateToProps, {})(Map);
